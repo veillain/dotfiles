fd708f64ebe246c7565c6e29a0b6a407c37c33bf
Veillain <fikri.mny@gmail.com>
2025-02-07 23:19:15 +0700

    Delete configs directory

--- configs/waybar/power/exec.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-id=$(ps -ef | grep '[w]aybar -c' | grep power | awk '{print $2}')
-
-case $1 in
-    lock) hyprlock; kill $id ;;
-esac
--- configs/waybar/power/toggle.sh
+++
@@ -1,9 +0,0 @@
-#!bin/bash
-
-id=$(ps -ef | grep '[w]aybar -c' | grep power | awk '{print $2}')
-
-if [ "$id" ]; then
-    kill $id & pkill -x rofi
-else 
-    waybar -c $HOME/.config/waybar/power/config.jsonc -s $HOME/.config/waybar/power/style.css
-fi
